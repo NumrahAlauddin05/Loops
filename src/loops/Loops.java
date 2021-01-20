7c7197654119ead2e9dcb51a25c56449d2caac7b
NAVTTC07 <numrah.alauddin@gmail.com>
2021-01-20 17:38:04 +0500

    loops intro

---
+++ src/loops/Loops.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loops;
+
+public class Loops {
+
+    public static void main(String[] args) {
+       //for ,while,do-while,for-each
+      
+        
+//        System.out.println("number "+num);
+//        System.out.println("number "+num);
+//        System.out.println("number "+num);
+//        System.out.println("number "+num);
+        
+        //syntax of for
+        //for( initialization; condition;inc/dec )1234{
+        //code
+        
+    //}
+    int c=10;
+    //int a=c++;
+    //iterations
+        for(int a=1; a<6 ; a++ ){//1,2,3,4,5
+             System.out.println("hello");
+        }
+        
+        for(int a=1 ; a<5 ;++a ){
+            System.out.println("abc");
+        }
+        int b;
+        for( b=1;b<5;b++){
+            
+            System.out.println("Iteration  "+b);//2 3 4
+        }
+        //Output
+        //2*1=2
+        //2*2=4
+         //int a=1;  
+         int num=5;
+            for (int i = 1; i <=10; i++) {
+            System.out.println(num+" * "+i+" = "+(num*i));
+            //a++;
+        }
+        
+       // System.out.println("a "+a);
+    }
+    
+}
